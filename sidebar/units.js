42a7fde57fabe5a193b5dc860f4b718d6c880174
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2020-11-22 15:20:10 +0100

    Units test

--- sidebar/units.js
+++ sidebar/units.js
@@ -3,10 +3,10 @@ var units =
 	"Unit 1: Introduction": {
 		"image": "/sidebar/what-block.svg",
 		"contents": {
-			"Lesson 1": {"url": "units/introduction/your_first_data_pack.html"},
-			"Lesson 2": {"url": "units/introduction/your_first_function.html"},
-			"Lesson 3": {"url": "units/introduction/ticks.html"},
-			"Lesson 4": {"url": "units/introduction/functions_with_delays.html"}
+			"Lesson 1": {"url": "/sidebar/units/introduction/your_first_data_pack.html"},
+			"Lesson 2": {"url": "/sidebar/units/introduction/your_first_function.html"},
+			"Lesson 3": {"url": "/sidebar/units/introduction/ticks.html"},
+			"Lesson 4": {"url": "/sidebar/units/introduction/functions_with_delays.html"}
 		}
 	},
 	"Unit 2": {
