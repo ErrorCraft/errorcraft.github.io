3f49c93beb274571b438cdcf176b5ba2577e7417
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-01-23 01:08:11 +0100

    Add multi-page test

---
+++ multi-page/script.js
@@ -0,0 +1,6 @@
+window.onload = async function() {
+	let testPage = "/test.html";
+	let html = await fetch(testPage).then(r => r.text());
+	let e = document.getElementById("output");
+	e.innerHTML = sidebarHTML;
+}
