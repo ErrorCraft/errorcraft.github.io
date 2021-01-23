4a11f434e4a8c79b7e08a39902b757911963f39f
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-01-23 01:09:37 +0100

    Update test script

--- multi-page/script.js
+++ multi-page/script.js
@@ -2,5 +2,5 @@ window.onload = async function() {
 	let testPage = "/test.html";
 	let html = await fetch(testPage).then(r => r.text());
 	let e = document.getElementById("output");
-	e.innerHTML = sidebarHTML;
+	e.innerHTML = html;
 }
