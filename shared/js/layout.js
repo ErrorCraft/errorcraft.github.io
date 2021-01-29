e737dccd0061113aca357fe26443c25718e0a44d
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-01-29 23:32:03 +0100

    Move function

--- shared/js/layout.js
+++ shared/js/layout.js
@@ -2,6 +2,8 @@ window.onload = async function() {
 	let pageLayout = document.getElementById("page-layout");
 	let layout = await getContents("/shared/html/layout");
 	pageLayout.innerHTML = layout;
+	
+	loadTheme();
 }
 
 async function getContents(url) {
