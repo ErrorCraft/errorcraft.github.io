20386f7c1909ed2e53912aec7e4e1380fc1eea1a
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-03 14:43:44 +0100

    Use URL instead of path when loading the module

--- shared/js/main.js
+++ shared/js/main.js
@@ -1,6 +1,6 @@
 //import * as Utilities from './utilities.js';
 
-export async function loadMain() {
+async function loadMain() {
 	//var navigationBar = await Utilities.getHtmlContents("/shared/html/navigation-bar");
 	//document.body.prepend(navigationBar);
 	console.log("a");
