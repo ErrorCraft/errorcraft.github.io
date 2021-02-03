d47663a0a6fe41bff5505c905732ac892b596cb2
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-03 14:42:16 +0100

    Use export in loadMain method

--- shared/js/main.js
+++ shared/js/main.js
@@ -1,6 +1,6 @@
 //import * as Utilities from './utilities.js';
 
-async function loadMain() {
+export async function loadMain() {
 	//var navigationBar = await Utilities.getHtmlContents("/shared/html/navigation-bar");
 	//document.body.prepend(navigationBar);
 	console.log("a");
