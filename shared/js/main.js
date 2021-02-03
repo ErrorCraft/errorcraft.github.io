06d61ac5898d9ec777d27e536bb06fd3b4790483
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-03 15:19:20 +0100

    Remove debug messages

--- shared/js/main.js
+++ shared/js/main.js
@@ -1,11 +1,8 @@
 import * as Utilities from './utilities.js';
 
 async function loadMain() {
-	console.log("a");
 	var navigationBar = await Utilities.getHtmlContents("/shared/html/navigation-bar");
 	document.body.prepend(navigationBar);
 }
 
-console.log("1");
 window.addEventListener('load', loadMain);
-console.log("2");
