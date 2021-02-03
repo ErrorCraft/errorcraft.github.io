be54177b535870a63cdc1a945e53ccf74b859e91
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-03 14:52:23 +0100

    Remove async part from loadMain method

--- shared/js/main.js
+++ shared/js/main.js
@@ -1,6 +1,6 @@
 //import * as Utilities from './utilities.js';
 
-async function loadMain() {
+function loadMain() {
 	//var navigationBar = await Utilities.getHtmlContents("/shared/html/navigation-bar");
 	//document.body.prepend(navigationBar);
 	console.log("a");
