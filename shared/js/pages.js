56c21e84cc3eb0b612a8ce54485beee58c277f2c
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-04 19:53:44 +0100

    Remove unused things and order things on the page

--- shared/js/pages.js
+++ shared/js/pages.js
@@ -1,6 +1,6 @@
 import * as Utilities from './utilities.js';
 
 export async function loadNavigationBar() {
-	document.body.prepend(await Utilities.getHtmlContents("/shared/html/navigation-bar"));
 	document.head.append(Utilities.getStylesheetElement("/shared/css/navigation-bar.css"));
+	document.body.prepend(await Utilities.getHtmlContents("/shared/html/navigation-bar"));
 }
