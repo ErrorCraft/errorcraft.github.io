8506773292a2b64f5bdaa00bf519a1dca6969d61
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-11 20:56:50 +0100

    Remove unused file and do more testing

--- shared/js/main.js
+++ shared/js/main.js
@@ -1,7 +1,9 @@
 import * as Pages from './pages.js';
 
 async function loadMain() {
+	console.log("loading...");
 	await Pages.loadLayout();
+	console.log("all (should be) done!");
 }
 
 window.addEventListener('load', loadMain);
