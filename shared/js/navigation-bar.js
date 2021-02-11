926368adb8bbb8fa3d61278b2855f0854384ac78
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-11 21:22:07 +0100

    Add test page again

--- shared/js/navigation-bar.js
+++ shared/js/navigation-bar.js
@@ -1,10 +1,6 @@
-import * as Utilities from './utilities.js';
-
 const navigationToggleButton = document.getElementsByClassName("nav-toggle")[0];
 const navigationContents = document.getElementsByClassName("nav-contents")[0];
 
 navigationToggleButton.addEventListener('click', function() {
 	navigationContents.classList.toggle('active');
 });
-
-document.head.append(Utilities.getStylesheetElement("/shared/css/navigation-bar-or.css"));
