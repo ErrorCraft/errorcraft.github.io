ec6c7696c7e72d94110bdbdc25b6ae4985bbe45e
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-12 16:12:15 +0100

    Add more debug messages

--- shared/js/navigation-bar.js
+++ shared/js/navigation-bar.js
@@ -4,6 +4,8 @@ const navigationToggleButton = document.getElementsByClassName("nav-toggle")[0];
 const navigationContents = document.getElementsByClassName("nav-contents")[0];
 
 console.log("b");
+console.log(navigationToggleButton);
+console.log(navigationContents);
 
 navigationToggleButton.addEventListener('click', function() {
 	console.log("d");
