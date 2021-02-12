8a21874351d1e55539d4ec67be7ad3a7f43f1b19
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-12 16:08:32 +0100

    Add log messages to navigation bar javascript

--- shared/js/navigation-bar.js
+++ shared/js/navigation-bar.js
@@ -1,6 +1,13 @@
+console.log("a");
+
 const navigationToggleButton = document.getElementsByClassName("nav-toggle")[0];
 const navigationContents = document.getElementsByClassName("nav-contents")[0];
 
+console.log("b");
+
 navigationToggleButton.addEventListener('click', function() {
+	console.log("d");
 	navigationContents.classList.toggle('active');
 });
+
+console.log("c");
