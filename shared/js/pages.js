ad278c8517c2443bc65a72b96f075251856d4adb
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-12 14:52:20 +0100

    Add css link to test page instead of loading it from javascript

--- shared/js/pages.js
+++ shared/js/pages.js
@@ -6,6 +6,6 @@ export async function loadLayout() {
 }
 
 async function loadNavigationBar() {
-	document.head.append(Utilities.getStylesheetElement("/shared/css/navigation-bar.css"));
+	//document.head.append(Utilities.getStylesheetElement("/shared/css/navigation-bar.css"));
 	document.body.prepend(await Utilities.getHtmlContents("/shared/html/navigation-bar"));
 }
