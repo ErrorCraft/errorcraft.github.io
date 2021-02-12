579b9c7b413e5eeb7bd8e195b30abff0edef72a9
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-12 16:45:04 +0100

    Remove comments and debug lines

--- shared/js/navigation-bar.js
+++ shared/js/navigation-bar.js
@@ -1,16 +1,6 @@
-console.log("a");
-
 const navigationToggleButton = document.getElementsByClassName("nav-toggle")[0];
 const navigationContents = document.getElementsByClassName("nav-contents")[0];
 
-console.log("b");
-console.log(navigationToggleButton);
-console.log(navigationContents);
-
 navigationToggleButton.addEventListener('click', function() {
-	console.log("d");
 	navigationContents.toggleAttribute('data-active');
-	//navigationContents.classList.toggle('active');
 });
-
-console.log("c");
