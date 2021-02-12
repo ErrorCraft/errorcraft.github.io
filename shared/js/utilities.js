511a52609e682d5fdbf405ed615ff7223625e233
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-12 16:45:49 +0100

    Sort methods based on export (public)

--- shared/js/utilities.js
+++ shared/js/utilities.js
@@ -3,10 +3,6 @@ export async function getHtmlContents(url) {
 	return new DOMParser().parseFromString(s, "text/xml").firstChild;
 }
 
-async function getContents(url) {
-	return await (await fetch(url)).text();
-}
-
 export function getStylesheetElement(url) {
 	var e = document.createElement('link');
 	e.href = url;
@@ -18,4 +14,8 @@ export function getScriptElement(url) {
 	var e = document.createElement('script');
 	e.src = url;
 	return e;
+}
+
+async function getContents(url) {
+	return await (await fetch(url)).text();
 }
