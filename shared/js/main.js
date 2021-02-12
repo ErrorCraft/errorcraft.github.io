414775d18cbcc8c5e9b33e28caaffe53ce8f320b
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-13 00:09:07 +0100

    Load favicon via javascript instead of adding them to the page

--- shared/js/main.js
+++ shared/js/main.js
@@ -2,8 +2,17 @@ import * as Pages from './pages.js';
 
 async function loadMain() {
 	console.log("loading...");
+	loadFavIcon();
 	await Pages.loadLayout();
 	console.log("all (should be) done!");
+}
+
+function loadFavIcon() {
+	var e = document.createElement('link');
+	e.rel = "shortcut icon";
+	e.type = "image/x-icon";
+	e.href = "favicon.png";
+	document.head.append(e);
 }
 
 window.addEventListener('load', loadMain);
