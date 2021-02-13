149eeb1debbe9dfdfc570df2f59cf8c6832393d5
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-02-13 16:23:23 +0100

    Remove navigation-bar.js as it is unused

--- shared/js/navigation-bar.js
+++
@@ -1,6 +0,0 @@
-const navigationToggleButton = document.getElementsByClassName("nav-toggle")[0];
-const navigationContents = document.getElementsByClassName("nav-contents")[0];
-
-navigationToggleButton.addEventListener('click', function() {
-	navigationContents.toggleAttribute('data-active');
-});
